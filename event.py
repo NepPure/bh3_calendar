01496de6a98c6db47c8e26759a5a950f8431348c
NepPure <oyyz@vip.qq.com>
2021-10-04 16:33:36 +0800

    从正文查找开始时间

--- event.py
+++ event.py
@@ -5,6 +5,8 @@ from dateutil.relativedelta import relativedelta
 import aiohttp
 import asyncio
 import math
+import functools
+import re
 
 # type 0 普通常驻任务深渊 1 新闻 2 蛋池 3 限时活动H5
 
@@ -37,7 +39,42 @@ ignored_ann_ids = [
 
 ]
 
+list_api = 'https://api-takumi.mihoyo.com/common/bh3_cn/announcement/api/getAnnList?game=bh3&game_biz=bh3_cn&lang=zh-cn&bundle_id=bh3_cn&platform=pc&region=ios01&level=1&channel_id=1&uid=51000000'
+detail_api = 'https://api-takumi.mihoyo.com/common/bh3_cn/announcement/api/getAnnContent?game=bh3&game_biz=bh3_cn&lang=zh-cn&bundle_id=com.miHoYo.bh3&platform=ios&region=ios01&level=88&channel_id=1'
 
+
+def cache(ttl=timedelta(hours=1), arg_key=None):
+    def wrap(func):
+        cache_data = {}
+
+        @functools.wraps(func)
+        async def wrapped(*args, **kw):
+            nonlocal cache_data
+            default_data = {"time": None, "value": None}
+            ins_key = 'default'
+            if arg_key:
+                ins_key = arg_key + str(kw.get(arg_key, ''))
+                data = cache_data.get(ins_key, default_data)
+            else:
+                data = cache_data.get(ins_key, default_data)
+
+            now = datetime.now()
+            if not data['time'] or now - data['time'] > ttl:
+                try:
+                    data['value'] = await func(*args, **kw)
+                    data['time'] = now
+                    cache_data[ins_key] = data
+                except Exception as e:
+                    raise e
+
+            return data['value']
+
+        return wrapped
+
+    return wrap
+
+
+@cache(ttl=timedelta(hours=3), arg_key='url')
 async def query_data(url):
     try:
         async with aiohttp.ClientSession() as session:
@@ -49,9 +86,13 @@ async def query_data(url):
 
 
 async def load_event_cn():
-    result = await query_data('https://api-takumi.mihoyo.com/common/bh3_cn/announcement/api/getAnnList?game=bh3&game_biz=bh3_cn&lang=zh-cn&bundle_id=bh3_cn&platform=pc&region=ios01&level=1&channel_id=1&uid=51000000')
-    if result and 'retcode' in result and result['retcode'] == 0:
+    result = await query_data(url=list_api)
+    detail_result = await query_data(url=detail_api)
+    if result and 'retcode' in result and result['retcode'] == 0 and detail_result and 'retcode' in detail_result and detail_result['retcode'] == 0:
         event_data['cn'] = []
+        event_detail = {}
+        for detail in detail_result['data']['list']:
+            event_detail[detail['ann_id']] = detail
         datalist = result['data']['list']
         for data in datalist:
             for item in data['list']:
@@ -76,6 +117,25 @@ async def load_event_cn():
                     item['start_time'], r"%Y-%m-%d %H:%M:%S")
                 end_time = datetime.strptime(
                     item['end_time'], r"%Y-%m-%d %H:%M:%S")
+
+                # 从正文中查找开始时间
+                if event_detail[item["ann_id"]] and '版本更新后' not in event_detail[item["ann_id"]]['content']:
+                    content = event_detail[item["ann_id"]]['content']
+                    searchObj = re.search(
+                        r'(\d{4,})?年?(\d+)月(\d+)日(\d+):(\d+)(?:(?:~|-)(?:\d{4,})?年?(?:\d+)月(?:\d+)日(?:\d+):(?:\d+))?', content, re.M | re.I)
+                    try:
+                        datelist = searchObj.groups()  # ('2021', '9', '17')
+                        if datelist and len(datelist) >= 5:
+                            year = datetime.now().year
+                            if datelist[0]:
+                                year = datelist[0]
+                            ctime = datetime.strptime(
+                                f'{year}-{datelist[1]}-{datelist[2]} {datelist[3]}:{datelist[4]}', r"%Y-%m-%d %H:%M")
+                            if ctime > start_time and ctime < end_time:
+                                start_time = ctime
+                    except Exception as e:
+                        pass
+
                 event = {'title': item['title'],
                          'start': start_time,
                          'end': end_time,
